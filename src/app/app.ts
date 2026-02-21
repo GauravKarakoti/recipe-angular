bc3750304531210ee363527ec4cd7780ae97bccf
Gaurav Karakoti <karakotigaurav12@gmail.com>
2026-02-21 12:11:42 +0530

    SSR

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Inject, OnInit, PLATFORM_ID } from '@angular/core';
 import { Header } from './header/header';
 import { RouterOutlet } from '@angular/router';
 import { CoreModule } from './core-module';
@@ -7,6 +7,7 @@ import { SharedModule } from './shared/shared-module';
 import { Store } from '@ngrx/store';
 import * as fromApp from './store/app.reducer';
 import * as AuthActions from './auth/store/auth.action';
+import { isPlatformBrowser } from '@angular/common';
 
 @Component({
   selector: 'app-root',
@@ -16,9 +17,11 @@ import * as AuthActions from './auth/store/auth.action';
   // providers: [LoggingService]
 })
 export class App implements OnInit {
-  constructor(private store: Store<fromApp.AppState>, private loggingService: LoggingService) {}
+  constructor(private store: Store<fromApp.AppState>, private loggingService: LoggingService, @Inject(PLATFORM_ID) private platformId: any) {}
   ngOnInit() {
-    this.store.dispatch(new AuthActions.AutoLogin());
+    if (isPlatformBrowser(this.platformId)) {
+      this.store.dispatch(new AuthActions.AutoLogin());
+    }
     this.loggingService.printLog("Welcome to recipe-angular")
   }
 }
